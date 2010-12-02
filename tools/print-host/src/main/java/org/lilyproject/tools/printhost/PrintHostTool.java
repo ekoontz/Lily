595795acf71d423ae45cc01cb52afe6c5710433c
bruno <bruno@b80d95f8-e103-0410-bd55-e03a9dc29394>
2010-12-02 15:07:49 +0000

    Add a tool to print the detected host name / ip address, that is used by Lily/Hadoop to publish for other nodes to connect to them. Include it in the binary dist.
    git-svn-id: http://dev.outerthought.org/svn_public/outerthought_lilyproject/trunk@4490 b80d95f8-e103-0410-bd55-e03a9dc29394

---
+++ tools/print-host/src/main/java/org/lilyproject/tools/printhost/PrintHostTool.java
@@ -0,0 +1,69 @@
+package org.lilyproject.tools.printhost;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.lilyproject.cli.BaseCliTool;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.util.List;
+
+public class PrintHostTool extends BaseCliTool {
+    protected Option nameserverOption;
+
+    @Override
+    protected String getCmdName() {
+        return "lily-print-host";
+    }
+
+    @Override
+    public List<Option> getOptions() {
+        List<Option> options = super.getOptions();
+
+        nameserverOption = OptionBuilder
+                .withArgName("nameserver")
+                .hasArg()
+                .withDescription("Non-default nameserver to use.")
+                .withLongOpt("host")
+                .create("n");
+
+        options.add(nameserverOption);
+
+        return options;
+    }
+
+    public static void main(String[] args) {
+        new PrintHostTool().start(args);
+    }
+
+    @Override
+    public int run(CommandLine cmd) throws Exception {
+        int result = super.run(cmd);
+        if (result != 0)
+            return result;
+
+        System.out.println("Below we print the detected host name and address.");
+        System.out.println("These are used by Lily and Hadoop. For example, this is what is");
+        System.out.println("published in ZooKeeper so that other nodes or clients can");
+        System.out.println("connect to this node.");
+        System.out.println();
+        System.out.println("If this shows localhost and 127.0.0.1, adjust your system setup.");
+        System.out.println();
+
+        String nameserver = "default";
+        if (cmd.hasOption(nameserverOption.getOpt())) {
+            nameserver = cmd.getOptionValue(nameserverOption.getOpt());
+        }
+
+        System.out.println("Using nameserver: " + nameserver);
+
+        String cn = InetAddress.getLocalHost().getCanonicalHostName();
+        System.out.println("Canonical host name: " + cn);
+
+        InetSocketAddress ad = new InetSocketAddress(cn, 1234);
+        System.out.println("Address of the canonical host name: " + ad.getAddress().getHostAddress());
+
+        return 0;
+    }
+}
