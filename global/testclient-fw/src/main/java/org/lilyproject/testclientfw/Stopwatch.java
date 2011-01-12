80d752eb2cbd09ce1c06dfcb1e3d2ef123cbdf16
bruno <bruno@b80d95f8-e103-0410-bd55-e03a9dc29394>
2011-01-12 08:20:53 +0000

    Adding a stopwatch class as replacement for calling System.nanoTime directly.
    git-svn-id: http://dev.outerthought.org/svn_public/outerthought_lilyproject/trunk@4552 b80d95f8-e103-0410-bd55-e03a9dc29394

---
+++ global/testclient-fw/src/main/java/org/lilyproject/testclientfw/Stopwatch.java
@@ -0,0 +1,44 @@
+package org.lilyproject.testclientfw;
+
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Class to measure elapsed time in milliseconds (but measured at nanosecond precision).
+ * One thread can only do one time-measurement at a time (it makes use of threadlocal state).
+ */
+public class Stopwatch {
+    private static final ThreadLocal<StopwatchData> dataTL = new ThreadLocal<StopwatchData>() {
+        @Override
+        protected StopwatchData initialValue() {
+            return new StopwatchData();
+        }
+    };
+
+    public static void start() {
+        StopwatchData data = dataTL.get();
+
+        if (data.started) {
+            LogFactory.getLog(Stopwatch.class).error("Stopwatch.start() called but it was already started.");
+        }
+
+        data.startedAt = System.nanoTime();
+        data.started = true;
+    }
+
+    public static double stop() {
+        StopwatchData data = dataTL.get();
+
+        if (!data.started) {
+            LogFactory.getLog(Stopwatch.class).error("Stopwatch.stop() called but it was not started.");
+        }
+
+        double duration = (System.nanoTime() - ((double)data.startedAt)) / 1e6;
+        data.started = false;
+        return duration;
+    }
+
+    private static class StopwatchData {
+        long startedAt;
+        boolean started;
+    }
+}
